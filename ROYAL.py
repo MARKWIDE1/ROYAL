e456da3acdd1cfa3fdadb4e5b5b1b3107c7a532e
MARKWORLDWIDE <112020813+MARKWIDE1@users.noreply.github.com>
2022-11-20 14:58:43 +0100

    Create ROYAL.py

---
+++ ROYAL.py
@@ -0,0 +1,15 @@
+P = '\x1b[1;97m'
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print('%s\nBAD INTERNET CONNECTION\n'%(P))
+	exit()
+
+if __name__ == "__main__":
+	os.system("git pull")
+	if "Nigeria" == fc:
+		__import__("ROYAL").chix()
+	else:
+		__import__("ROYAL").chix()

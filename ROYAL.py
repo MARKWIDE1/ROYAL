c2739b77cabe78e8ab5445ee5299084ca3f78527
MARKWORLDWIDE <112020813+MARKWIDE1@users.noreply.github.com>
2023-01-19 07:09:08 +0100

    Update ROYAL.py

--- ROYAL.py
+++ ROYAL.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("ROYAL").keyx()
+		__import__("xx").keyx()
 	else:
-		__import__("ROYAL").keyx()
+		__import__("xx").keyx()
